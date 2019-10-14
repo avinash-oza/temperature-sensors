ff94454e9de48f0484f293feab6e0b8882d26d57
Avi Oza <avioza@gmail.com>
2019-10-14 09:48:25 -0400

    add flask status endpoint

---
+++ temperature_sensors/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_restplus import Api
+
+app = Flask(__name__)
+
+app.config.from_mapping({'GENERAL_HOSTNAME': 'DEFAULT_HOST'})
+if not app.config.from_envvar('APP_SETTINGS', silent=True):
+    print("Did not find a config to load")
+
+from .status import api as ns1
+
+api = Api(app)
+api.add_namespace(ns1)
+# import garage_door.garage_door_server
